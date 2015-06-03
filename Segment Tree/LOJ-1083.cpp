f8ba6f6416822fd6046a6706d67622b366f05143
Nazmul Islam <nazmulcuet11@gmail.com>
2015-06-03 18:06:19 +0600

    LOJ-1083 to Segment Tree

---
+++ Segment Tree/LOJ-1083.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define SIZE 30050
+
+ll testCase,caseNo = 1,n,arr[SIZE],tree[SIZE*3];
+
+void init(ll node,ll b,ll e)
+{
+
+}
+
+int main()
+{
+    scanf("%lld",&testCase);
+    while(testCase--)
+    {
+        scanf("%lld",&n);
+        for(ll i=1;i<=n;i++)
+            scnaf("%lld",&arr[i]);
+
+
+    }
+    return 0;
+}
